bcebf5c13e06b48c7190aad28adc6b633fb7c653
Paula <paulavidas@gmail.com>
2019-10-15 15:54:41 +0200

    promjene u skripti za testiranje

--- 1/trobojnica/testiraj.sh
+++ 1/trobojnica/testiraj.sh
@@ -1,10 +1,20 @@
 program=$1
 my_out=tmp_out
+points_file=points.txt
 
-for test_in in test/trobojnica.in.*; do
-    test_out=${test_in/in/out}
-    ./$program < $test_in > $my_out
-    ./checker $test_in $test_out $my_out
+for i in 1 2 3; do
+    subtask_points=1
+    for test_in in test/trobojnica.in.$i*; do
+        test_out=${test_in/in/out}
+        ./$program < $test_in > $my_out
+        ./checker $test_in $test_out $my_out > $points_file
+        points=$(head -n 1 $points_file)
+        if (( $(echo "$points < $subtask_points" | bc -l) )); then
+            subtask_points=$points
+        fi
+        head -2 $points_file | tail -1
+    done
+    echo === subtask \#$i $subtask_points
 done
 
-rm $my_out
+rm $my_out $points_file
