45df948503015943af2429d6366fe4af46898be5
Marin <marinkisic14@gmail.com>
2019-11-12 21:04:02 +0100

    Dodano rjesenje i update generatora

--- 2/zvijezda/gen.cpp
+++ 2/zvijezda/gen.cpp
@@ -18,40 +18,6 @@ typedef pair <ll, ll> pll;
 #define mp make_pair
 #define pb push_back
 
-const lf EPS = 1e-12;
-
-inline bool lt(const lf &a, const lf& b) { return b - a < EPS; }
-inline bool eq(const lf &a, const lf& b) { return !lt(a, b) && !lt(b, a); }
-
-typedef ll T;
-struct pt {
-  T x,y;
-  pt() {}
-  pt(pii t) {
-    x = (T)t.fi;
-    y = (T)t.sec;
-  }
-  pt(pll t) {
-    x = (T)t.fi;
-    y = (T)t.sec;
-  }
-  pt(T a, T b) {
-    x = (T)a;
-    y = (T)b;
-  }
-  pt operator-(pt p) {return pt(x-p.x, y-p.y);}
-
-  friend bool operator < (pt a, pt b) {
-    if (!eq(a.x, b.x)) return lt(a.x, b.x);
-    return lt(a.y, b.y);
-  }
-};
-
-bool operator==(pt a, pt b) {return eq(a.x, b.x) && eq(a.y, b.y);}
-bool operator!=(pt a, pt b) {return !(a == b);}
-
-lf cross(pt v, pt w) {return (lf)v.x*(lf)w.y - (lf)v.y*(lf)w.x;}
-lf ccw(pt a, pt b, pt c) {return cross(b-a,c-a);}
 
 const vector <string> cluster[3] = {
   {"1a", "1b", "1c", "1d", "2a", "2b", "2c", "2d", "2e", "2f", "2g"},
@@ -79,43 +45,38 @@ ll RandLL(ll x = (1LL << 60)) {
 int n;
 vector <pii> poly;
 
-bool get(int x, ll a, ll b) {
-  return !lt(ccw(pt(poly[x]), pt(poly[(x + 1)%n]), pt(a, b)), 0);
+lf ccw(pll a, pll b, pll c) {
+  return (lf)a.fi * (b.sec - c.sec) +
+         (lf)b.fi * (c.sec - a.sec) +
+         (lf)c.fi * (a.sec - b.sec);
+}
+
+bool get(int i, ll x, ll y) {
+  return ccw(poly[i], poly[(i+1)%n], {x, y}) >= 0;
+}
+
+int mjeri(int l, int r, ll x, ll y) {
+  int lo = l, hi = r, mid;
+  while (lo != hi) {
+    mid = (lo + hi + 1) / 2;
+    if (get(mid, x, y) == get(l, x, y)) lo = mid;
+    else hi = mid - 1;
+  }
+  int ret = lo - l + 1;
+  if (!get(l, x, y)) ret = n/2 - ret;
+  return ret;
 }
 
 bool pitaj(ll x, ll y) {
   int a = get(0, x, y);
   int b = get(n/2, x, y);
   if (a == b) return a;
-  int lo = 0, hi = n/2-1, mid;
-  while (lo != hi) {
-    mid = (lo + hi + 1) / 2;
-    if (get(mid, x, y) == a) lo = mid;
-    else hi = mid - 1;
-  }
-  int L1 = 0, R1 = lo;
-  lo = n/2;
-  hi = n - 1;
-  while (lo != hi) {
-    mid = (lo + hi + 1) / 2;
-    if (get(mid, x, y) == b) lo = mid;
-    else hi = mid - 1;
-  }
-  int L2 = n/2, R2 = lo;
-  int imam = 0;
-  if (a == 1) {
-    imam += R1 - L1 + 1;
-    imam += n/2 - (R2 - L2 + 1);
-  } else {
-    imam += R2 - L2 + 1;
-    imam += n/2 - (R1 - L1 + 1);
-  }
-  return imam >= n/2+1;
+  return mjeri(0, n/2 - 1, x, y) + mjeri(n/2, n - 1, x, y) > n / 2;
 }
 
 int CNT;
 
-pii DA() {
+pll get_point(int pozitiv) {
   pll ret;
   int cnt = 0;
   do {
@@ -124,21 +85,7 @@ pii DA() {
     ret.sec = RandLL();
     if (Rand(2)) ret.fi = -ret.fi;
     if (Rand(2)) ret.sec = -ret.sec;
-  } while (!pitaj(ret.fi, ret.sec));
-  CNT = max(CNT, cnt);
-  return ret;
-}
-
-pii NE() {
-  pll ret;
-  int cnt = 0;
-  do {
-    cnt++;
-    ret.fi = RandLL();
-    ret.sec = RandLL();
-    if (Rand(2)) ret.fi = -ret.fi;
-    if (Rand(2)) ret.sec = -ret.sec;
-  } while (!pitaj(ret.fi, ret.sec));
+  } while (pitaj(ret.fi, ret.sec) != pozitiv);
   CNT = max(CNT, cnt);
   return ret;
 }
@@ -167,9 +114,7 @@ void napravi(int test, string OUT, string IN) {
   int da = 0;
   REP(i, q) {
     int ok = Rand(2);
-    pll tocka;
-    if (ok) tocka = DA();
-    else tocka = NE();
+    pll tocka = get_point(ok);
     if (HASH[test]) tocka.fi ^= da;
     if (HASH[test]) tocka.sec ^= da;
     da += ok;
