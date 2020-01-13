0acc6e01112f4bef60f657fc6036838801e38407
Marin <marinkisic14@gmail.com>
2020-01-13 19:11:44 +0100

    Dodan sesti zad

---
+++ 4/interval/marin.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double lf;
+typedef long double Lf;
+typedef pair <int,int> pii;
+typedef pair <ll, ll> pll;
+
+#define TRACE(x) cerr << #x << "  " << x << endl
+#define FOR(i, a, b) for (int i = (a); i < int(b); i++)
+#define REP(i, n) FOR(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+#define _ << " " <<
+
+#define fi first
+#define sec second
+#define mp make_pair
+#define pb push_back
+
+const int MAXN = 110;
+const int MAXK = MAXN * MAXN;
+const int inf = (1 << 17);
+
+int n, p[MAXN], L, R, K;
+int dpL[MAXN][MAXK], dpR[MAXN][MAXK];
+
+vector <int> dp[3][MAXK];
+
+void compute(int t) {
+  int left = L;
+  int right = R;
+  if (t) {
+    left = n - R - 1;
+    right = n - L - 1;
+    reverse(p, p + n);
+  }
+
+  REP(i, 3) REP(j, MAXK) dp[i][j].clear();
+
+  REP(k, K + 1) {
+    FOR(i, 1, n) {
+      if (i == left) dp[0][k].pb(0);
+      else dp[0][k].pb(-inf);
+    }
+  }
+
+  REP(k, K + 1) {
+    REP(i, n) {
+      if (i == left || i == left - 1) dp[1][k].pb(0);
+      else dp[1][k].pb(-inf);
+    }
+  }
+
+  FOR(len, 2, n + 1) {
+    int x = len % 3;
+    REP(k, K + 1) {
+      dp[x][k].clear();
+      REP(i, n - len + 1) {
+        int j = i + len - 1;
+        int curr = -inf;
+        curr = max(curr, dp[(x + 2) % 3][k][i + 1]);
+        curr = max(curr, dp[(x + 2) % 3][k][i]);
+        if (k - abs(i - j) >= 0) curr = max(curr, dp[(x + 1) % 3][k - abs(i - j)][j - 1] + p[j] - p[i]);
+        dp[x][k].pb(curr);
+        if (i == 0 && j >= left && j <= right) {
+          if (t == 0) dpL[j][k] = curr;
+          if (t == 1) dpR[n - j - 1][k] = curr;
+        }
+      }
+    }
+  }
+
+  if (t) {
+    reverse(p, p + n);
+  }
+}
+
+int merge(int x) {
+  int ret = 0;
+  REP(k, K + 1) {
+    ret = max(ret, dpL[x][k] + dpR[x + 1][K - k]);
+  }
+  return ret;
+}
+
+int main() {
+  scanf("%d %d %d %d",&n,&L,&R,&K);
+  L--; R--;
+  REP(i, n) scanf("%d",&p[i]);
+
+  REP(i, 2) compute(i);
+  int sol = 0;
+  FOR(i, L, R + 1) sol = max(sol, merge(i));
+
+  int sum = 0;
+  FOR(i, L, R + 1) sum += p[i];
+  printf("%d\n",sum - sol);
+  return 0;
+}
+
