36a7e5b27095e0f7fb7e626eb97c9b69b3b9d89e
Paula <paulavidas@gmail.com>
2020-02-02 14:22:58 +0100

    promjena primjera

--- 5/emacs/gen.sh
+++ 5/emacs/gen.sh
@@ -6,9 +6,9 @@
 ./gen 1 95 1000 0 > test/emacs.in.5
 
 ./gen 100 100 1000 0 > test/emacs.in.6
-./gen 95 100 1000 0 > test/emacs.in.7
-./gen 100 95 1000 0 > test/emacs.in.8
-./gen 100 100 1 2 > test/emacs.in.9
-./gen 100 100 0 0 > test/emacs.in.10
+./gen 100 100 10000 0 > test/emacs.in.7
+./gen 95 100 1000 0 > test/emacs.in.8
+./gen 100 95 1000 0 > test/emacs.in.9
+./gen 100 100 1 2 > test/emacs.in.10
 
 for i in {1..10}; do ./paula < test/emacs.in.$i > test/emacs.out.$i; done
